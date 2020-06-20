59d8a6bac1b3222fa1aa927e36c53ad037ed60d7
FatimaAbdimalik <43494317+FatimaAbdimalik@users.noreply.github.com>
2020-06-20 16:33:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# tv-show-dom-project
+
+A starting point for CYF's TV show DOM project
+
+The requirements for the project are here:
+
+https://github.com/CodeYourFuture/syllabus/tree/master/js-core-3/tv-show-dom-project/readme.md
---
+++ script.js
@@ -0,0 +1,12 @@
+//You can edit ALL of the code here
+function setup() {
+  const allEpisodes = getAllEpisodes();
+  makePageForEpisodes(allEpisodes);
+}
+
+function makePageForEpisodes(episodeList) {
+  const rootElem = document.getElementById("root");
+  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+}
+
+window.onload = setup;
