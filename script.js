9e01733a2ad7432a5a597f4cb273e0d41e21075e
Fatima Abdimalik <f.abdimalik@gmail.com>
2020-09-01 19:26:51 +0100

    level 300 completed

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 //You can edit ALL of the code here
-const allEpisodes = getAllEpisodes();
 let body = document.getElementById("background");
-body.style.backgroundColor = "#D3D3D3";
 const cardContainer = document.getElementById("cardContainer");
 const rootElem = document.getElementById("root");
 const displayNum = document.getElementById("display");
-
+let allEpisodes = [];
+let pickAnEpisode = getAllEpisodes();
 function setup() {
-  makePageForEpisodes(allEpisodes);
+  console.log("from setup");
+  fetch("https://api.tvmaze.com/shows/82/episodes")
+    .then((res) => res.json())
+    .then((data) => {
+      allEpisodes = data;
+      makePageForEpisodes(allEpisodes);
+    });
 }
-
 function makePageForEpisodes(episodeList) {
   cardContainer.innerHTML = "";
   let numOfEp = episodeList.length;
-  displayNum.innerHTML = `Displaying ${numOfEp}/73 episodes`;
+  displayNum.innerHTML = `Displaying ${numOfEp}/${numOfEp} episodes`;
   // create a div card first then the rest then appenchild the rest
   episodeList.forEach((element) => {
     let Div = document.createElement("div");
@@ -28,15 +32,6 @@ function makePageForEpisodes(episodeList) {
     titleDiv.style.textAlign = "center";
     titleDiv.style.paddingTop = "8px";
     titleDiv.style.paddingBottom = "8px";
-    // Div.style.width = "20rem";
-    // Div.style.height = "24rem";
-    // Div.style.marginTop = "50px";
-    // Div.style.marginLeft = "35px";
-    // Div.style.marginBottom = "50px";
-    // Div.style.backgroundColor = "white";
-    // Div.style.padding = "5px 10px 10px";
-    // Div.style.borderRadius = "15px";
-    // Div.style.border = "10px solid black";
     let epsName = document.createElement("h4");
     let epSummary = document.createElement("p");
     let epImage = document.createElement("img");
@@ -64,31 +59,38 @@ function findValue() {
   let episodes = allEpisodes.filter((ep) =>
     (ep.name + ep.summary).toLowerCase().includes(values)
   );
+
   makePageForEpisodes(episodes);
   console.log(`Displaying ${episodes.length}/73 episodes`);
+  // if (episodes) {
+  //   console.log(`episodes from episodes, ${episodes}`);
+  //   values.style.color = "yellow";
+  // }
   return (displayNum.innerHTML = `Displaying ${episodes.length}/73 episodes`);
 }
 // level 300
 let select = document.getElementById("episodes");
-let data = allEpisodes;
-data
-  .map((p) => `S${addPad(p.season)}E${addPad(p.number)} - ${p.name}`)
-  .forEach((item) => {
-    let option = document.createElement("option");
-    option.innerHTML = item;
-    select.appendChild(option);
-  });
+let data = pickAnEpisode;
+data.forEach((item) => {
+  let option = document.createElement("option");
+  option.value = item.id;
+  option.innerHTML = `S${addPad(item.season)}E${addPad(item.number)} - ${
+    item.name
+  }`;
+  select.appendChild(option);
+});
+console.log(select);
+console.log(data);
 
 select.addEventListener("change", selectEpisode);
 function selectEpisode() {
   let valueOfSele = select.value;
   console.log(valueOfSele);
-
-  let selectedEp = allEpisodes.filter((ep) => valueOfSele.includes(ep.name));
-  console.log(selectedEp);
+  let selectedEp = data.filter((ep) => ep.id == valueOfSele);
+  // console.log(selectedEp);
 
   if (valueOfSele === "all") {
-    makePageForEpisodes(allEpisodes);
+    makePageForEpisodes(data);
   } else {
     makePageForEpisodes(selectedEp);
   }
